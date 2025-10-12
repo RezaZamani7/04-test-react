ce581c93c02cc756e582c48956cc211d442c1e84
RezaZamani7 <reza.zamani777@gmail.com>
2025-10-12 10:44:51 +0330

    Products functional component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Product from './components/functional/product';
+import Products from './components/functional/products';
 import { createRoot } from 'react-dom/client';
 
 const rootElement=document.getElementById('root');
 const root=createRoot(rootElement);
-root.render(<Product/>)
+root.render(<Products/>)
